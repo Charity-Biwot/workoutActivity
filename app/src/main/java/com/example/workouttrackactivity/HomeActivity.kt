04a35ffba903504cee12a6b1aa487123f781c50e
Charity-Biwot <biwotcharityjerop@gmail.com>
2022-06-14 17:40:32 +0300

    commit

--- app/src/main/java/com/example/workouttrackactivity/HomeActivity.kt
+++ app/src/main/java/com/example/workouttrackactivity/HomeActivity.kt
@@ -1,16 +1,11 @@
 package com.example.workouttrackactivity
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class splash : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val intent = Intent(this,LoginActivity::class.java)
-      startActivity(intent)
-      finish()
-
-        setContentView(R.layout.activity_splash)
+        setContentView(R.layout.activity_home)
     }
 }
